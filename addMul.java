2961529f8240edbe6af83b331660ddccf5a12252
kumar-Rakesh <kumrakesh16453@gmail.com>
2017-05-16 16:44:58 +0530

    addition and multiplication of three numbers

---
+++ addMul.java
@@ -0,0 +1,9 @@
+class addMul{
+   public static void main(String args[]){
+     int a=10;
+     int b=5;
+     int c=10;
+     System.out.println(""+(a+b+c));
+     System.out.println(""+(a*b*c));
+   }
+ 
